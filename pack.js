7872e18cfb62bf5ee6ee4596bd654acfe755eeba
叶亮 <yeliang@83chedai.com>
2017-09-08 17:27:29 +0800

    车辆显示

---
+++ pack.js
@@ -0,0 +1,37 @@
+var exec = require('child_process').exec;
+
+var isEnviroment = process.argv[2];
+
+var enviroment = [
+	{
+	  webpack: { 'dev': 'npm run dev', 'production': 'npm run build'},
+	  name:'webpack'
+  },
+	{
+	 gulp: {'dev': 'gulp',  'production': 'gulp ejs sass'},
+	 name:'gulp'
+  },
+	{
+	 node: {'dev': 'pm2 kill && npm start', 'production': 'pm2 kill && npm run production'},
+	 name:'node'
+ }
+];
+
+enviroment.forEach((item) => {
+
+	console.log(item[item.name][isEnviroment])
+
+	exec(item[item.name][isEnviroment],(err, stdout, stderr) =>{
+
+		if (err) {
+
+			throw new Error(stdout);
+		}
+
+		console.log(stdout);
+
+	})
+
+})
+
+
