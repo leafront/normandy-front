6e3bf3a6572c431da5fcb1e62992577139adbfeb
叶亮 <yeliang@83chedai.com>
2017-09-22 13:38:20 +0800

    modify release.js

--- release.js
+++ release.js
@@ -6,15 +6,26 @@ conn.on('ready', function() {
 
 	conn.exec('/usr/share/nginx/normandy_front/front/release.sh', function(err, stream) {
 
-			if (err) throw err;
-			stream.on('close', function (code, signal) {
-				console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
-				conn.end();
-			}).on('data', function (data) {
-				console.log('STDOUT: ' + data);
-			}).stderr.on('data', function (data) {
-				console.log('STDERR: ' + data);
-			})
+		if (err) {
+
+			throw err;
+		}
+
+		stream.on('close', function (code, signal) {
+
+			console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+
+			conn.end();
+
+		}).on('data', function (data) {
+
+			console.log('STDOUT: ' + data);
+
+		}).stderr.on('data', function (data) {
+
+			console.log('STDERR: ' + data);
+
+		})
 
 	})
 
