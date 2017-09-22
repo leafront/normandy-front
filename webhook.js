5658a29f6302a17347779228004063352b9413da
叶亮 <yeliang@83chedai.com>
2017-09-23 01:33:14 +0800

    modified webhook.js

--- webhook.js
+++ webhook.js
@@ -1,29 +1,33 @@
 var http = require('http')
-var createHandler = require('github-webhook-handler')
-var handler = createHandler({ path: '/webhook', secret: 'leafront' })
+var createHandler = require('coding-webhook-handler')
+var handler = createHandler({
+	path: '/webhook',
+	token: 'leafront' // maybe there is no token
+})
 
-http.createServer(function (req, res) {
-	handler(req, res, function (err) {
+http.createServer(function(req, res) {
+	handler(req, res, function(err) {
 		res.statusCode = 404
 		res.end('no such location')
 	})
 }).listen(7777)
 
-handler.on('error', function (err) {
+handler.on('error', function(err) {
 	console.error('Error:', err.message)
 })
 
-handler.on('push', function (event) {
-
-	console.log('Received a push event for %s to %s',
-		event.payload.repository.name,
-		event.payload.ref)
+handler.on('*', function(event) {
+	console.log(event.event)
+	console.log(event.payload)
+	console.log(event.protocol)
+	console.log(event.host)
+	console.log(event.url)
 })
 
-handler.on('issues', function (event) {
-	console.log('Received an issue event for %s action=%s: #%d %s',
-		event.payload.repository.name,
-		event.payload.action,
-		event.payload.issue.number,
-		event.payload.issue.title)
+handler.on('push', function(event) {
+	console.log(event)
+})
+
+handler.on('star', function(event) {
+	console.log(event)
 })
