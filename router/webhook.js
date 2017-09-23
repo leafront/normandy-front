b5960b1f7ad9006f1a73b1dca3e3e2ba33e57108
叶亮 <yeliang@83chedai.com>
2017-09-23 16:08:30 +0800

    modified webhook.js

--- router/webhook.js
+++ router/webhook.js
@@ -6,13 +6,20 @@ router.post('/', async (ctx,next) => {
 
 	const header = ctx.request.header;
 
+	const body = ctx.request.body;
+
+	const repository = body.repository.url;
+
+	const branchName = body.ref;
+
+
 	const token = header["x-gitlab-token"];
 
 	if (token == "leafront") {
 
 		ctx.body = 'release success';
 
-		exec('/usr/share/nginx/normandy_front/front/release.sh',(err, stdout, stderr) =>{
+		exec(`/usr/share/nginx/normandy_front/front/release.sh ${repository} ${branchName}`,(err, stdout, stderr) =>{
 
 			if (err) {
 
