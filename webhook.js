8484738ad6b8bf8c5418e87ede88bb6f8a1a669a
叶亮 <yeliang@83chedai.com>
2017-09-23 10:44:48 +0800

    modified webhook.js

--- webhook.js
+++ webhook.js
@@ -1,9 +1,4 @@
 var http = require('http')
-var createHandler = require('coding-webhook-handler')
-var handler = createHandler({
-	path: '/webhook',
-	token: 'leafront' // maybe there is no token
-})
 
 http.createServer(function(req, res) {
 	handler(req, res, function(err) {
@@ -12,22 +7,3 @@ http.createServer(function(req, res) {
 	})
 }).listen(7777)
 
-handler.on('error', function(err) {
-	console.error('Error:', err.message)
-})
-
-handler.on('*', function(event) {
-	console.log(event.event)
-	console.log(event.payload)
-	console.log(event.protocol)
-	console.log(event.host)
-	console.log(event.url)
-})
-
-handler.on('push', function(event) {
-	console.log(event)
-})
-
-handler.on('star', function(event) {
-	console.log(event)
-})
