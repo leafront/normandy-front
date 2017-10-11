0dda9e506294a4821665850b61c9bdfad9dcf682
叶亮 <yeliang@83chedai.com>
2017-10-11 10:38:04 +0800

    add release.js

---
+++ release.js
@@ -0,0 +1,42 @@
+var Client = require('ssh2').Client;
+
+var conn = new Client();
+
+var exec = require('child_process').exec;
+
+conn.on('ready', function() {
+
+	conn.exec('/usr/share/nginx/normandy_front/front/release.sh', function(err, stream) {
+		if (err) {
+
+			if (err) {
+				throw new Error(err);
+
+				throw err;
+			}
+		}
+
+		stream.on('close', function (code, signal) {
+
+			console.log('Stream :: close :: code: ' + code + ', signal: ' + signal);
+
+			conn.end();
+
+		}).on('data', function (data) {
+
+			console.log('STDOUT: ' + data);
+
+		}).stderr.on('data', function (data) {
+
+			console.log('STDERR: ' + data);
+
+		})
+	})
+
+}).connect({
+		host: '116.62.246.98',
+		port: 22,
+		username: 'root',
+		privateKey: require('fs').readFileSync('/Users/leafrontye/.ssh/id_rsa')
+})
+
